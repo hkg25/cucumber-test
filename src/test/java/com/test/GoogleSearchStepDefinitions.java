c4868cc583a928e7fb69c0c332cf6f75a7cc3a4f
Harish Garg <hgarg@sapient.com>
2016-08-15 23:17:35 -0700

    Problem resolved using capabilities.

--- src/test/java/com/test/GoogleSearchStepDefinitions.java
+++ src/test/java/com/test/GoogleSearchStepDefinitions.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -21,16 +22,24 @@ import cucumber.api.java.en.When;
  */
 public class GoogleSearchStepDefinitions {
 
+	private static WebDriver driver = null;
+
 	static {
-		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+
 		System.setProperty("webdriver.ie.driver",
 				"C:\\Users\\hgarg\\Downloads\\IEDriverServer_x64_2.53.1\\IEDriverServer.exe");
-		
+
 		System.out.println(System.getProperty("webdriver.ie.driver"));
+		DesiredCapabilities capabilities = DesiredCapabilities
+				.internetExplorer();
+		capabilities
+				.setCapability(
+						InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+						true);
+		driver = new InternetExplorerDriver(capabilities);
 	}
 
-	private final WebDriver driver = new InternetExplorerDriver();
+	// private final WebDriver driver = new InternetExplorerDriver();
 
 	@Given("^I am on the Google search page$")
 	public void I_visit_google_page() {
@@ -40,24 +49,30 @@ public class GoogleSearchStepDefinitions {
 
 	@When("^I search for \"(.*)\"$")
 	public void search_For(String query) {
+
 		WebElement element = driver.findElement(By.name("q"));
 		element.sendKeys(query);
 		element.submit();
+
 		System.out.println("On when condition");
 	}
 
 	@Then("^the page title should start with \"(.*)\"$")
 	public void checkTitle(String str) {
+
 		new WebDriverWait(driver, 10).until(new ExpectedCondition<Boolean>() {
 			public Boolean apply(WebDriver d) {
 				return d.getTitle().toLowerCase().startsWith("cheese");
 			}
 		});
+
 		// assertEquals(driver.getTitle(),startsWith("cheese"));
 		System.out.println("On then condition");
 	}
 
-	/*
-	 * @After public void closeBrowser(){ driver.quit(); }
-	 */
+	@After
+	public void closeBrowser() {
+		driver.quit();
+	}
+
 }
